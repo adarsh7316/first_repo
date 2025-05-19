d2db08475d978a97e56eee9f2788b7c73e10617e
adarsh <adarshh7439@gmail.com>
2025-05-19 09:23:08 +0530

    added new line character

--- details.c
+++ details.c
@@ -3,11 +3,11 @@ int main()
 {
 char s[20];
 int a;
-printf("enter your name");
+printf("enter your name\n");
 fgets(s,sizeof(s),stdin);
-printf("enter your marks");
+printf("enter your marks\n");
 scanf("%d",&a);
-printf("%d",a);
-printf("%s",s);
+printf("%d\n",a);
+printf("%s\n",s);
 return 0;
 }
